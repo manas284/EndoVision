2fd6975d5b33f48a7efd1a450673e453d9da1a42
Dr Ram Ji Tiwari <ramjitiwari453@gmail.com>
2025-07-28 10:37:38 +0000

    my image is not shown

--- next.config.ts
+++ next.config.ts
@@ -32,12 +32,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'kenmedsurgical.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
 };
