ba454441669bf87367f8136f9f293a643a0114ca
Dr Ram Ji Tiwari <ramjitiwari453@gmail.com>
2025-07-28 10:28:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -28,6 +29,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kenmedsurgical.com',
         port: '',
         pathname: '/**',
       },
